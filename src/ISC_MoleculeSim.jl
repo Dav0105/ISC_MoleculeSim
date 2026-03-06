edb0e3e27db9abb96450255ce9b0adcf7a217d75
Dav0105 <120047730+Dav0105@users.noreply.github.com>
2026-03-06 14:18:08 +0100

    Started implementing collision handling
    Also fixed animation not rendering all time :)

--- src/ISC_MoleculeSim.jl
+++ src/ISC_MoleculeSim.jl
@@ -1,6 +1,6 @@
 module ISC_MoleculeSim
 
-using GLMakie
+using GLMakie, LinearAlgebra, Makie
 
 """
 Struct used to store Molecule properties
@@ -21,28 +21,70 @@ mutable struct Molecule
     speed::Vector       # m/s
 end
 
-"""Compute next position."""
-function computeNextPosition(m::Molecule, dt::Number)::Vector
+"""Computes the Movement of a molecule with delta_time provided."""
+function computeMovement(m::Molecule, dt::Number)
     m.position = m.position .+ (m.speed * dt)
     return m.position
 end
 
-"""Compute all positions for a molecule until provided time."""
-function computePositions(m::Molecule, dt::Number, until::Number)::Vector
+"""Function called when there is a collision. """
+function computeCollisionVelocity(m1::Molecule, m2::Molecule)
+    n::Vector = (m1.position .- m2.position) / norm(m1.position .- m2.position)
+    v1::Vector = m1.speed .- ((2 * m2.mass) / (m1.mass + m2.mass)) .* ((m1.speed .- m2.speed) .* n) .* n
+    v2::Vector = m2.speed .+ ((2 * m1.mass) / (m1.mass + m2.mass)) .* ((m1.speed .- m2.speed) .* n) .* n
+
+    m1.speed = v1
+    m2.speed = v2
+end
+
+"""Compute next position for all molecules."""
+function computeNextPosition(m::Molecule, mols::Array{Molecule}, dt::Number)::Vector
+    # m.position = m.position .+ (m.speed * dt)
+    # return m.position
+
+    # Check collisions for this molecule with others
+    collides_with = Nothing
+    for o_m::Molecule in mols
+        if (m == o_m) 
+            break
+        end
+
+        if (detectCollision(m, o_m))
+            # Collision detected
+            collides_with = o_m
+            break
+        end
+    end
+
+    if (collides_with != Nothing)
+        println("COLLISION DETECTED!!?!?!??!??!?!?!?!?!?!?")
+        computeCollisionVelocity(m, collides_with)
+    end
+    computeMovement(m, dt)
+
+    return m.position
+end
+
+"""Compute all positions for all molecules until provided time."""
+function computePositions(mols::Array{Molecule}, dt::Number, until::Number)::Vector
     pos::Vector = []
 
-    for i in 0:dt:until
-        newPos = computeNextPosition(m, dt)
-        push!(pos, newPos)
+    for m in mols
+        m_pos::Vector = []
+        for i in 0:dt:until
+            newPos = computeNextPosition(m, mols, dt)
+            push!(m_pos, newPos)
+        end
+        push!(pos, m_pos)
     end
 
     return pos
 end
 
+"""Returns True if m1 overlaps m2."""
 function detectCollision(m1::Molecule, m2::Molecule)::Bool
     r = m1.radius + m2.radius
-    println(m1.radius)
-    x1, y1, z1 = m1.position
+    x1, y1, z1 = m1.position 
     x2, y2, z2 = m2.position
     dist = sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)
 
@@ -70,21 +112,24 @@ function main()
     u = 1.660_538_921 * (10^-27) # [kg]
     pico = 10^-12
     nano = 10^-9
-    mHe  = Molecule("He", 4.002_602 * u      , 123 * pico        , [0, 0, 0], [0.001, 0, 0])
-    mNe  = Molecule("Ne", 20.179_70 * u      , 38  * pico        , [0, 0, 0], [0, 0, 0.001])
-    mN_2 = Molecule("N2", 14.006_70 * 2 * u  , (0.315 / 2) * nano, [0, 0, 0], [0, 0, 0.001])
-    mO_2 = Molecule("O2", 15.999_40 * 2 * u  , (0.292 / 2) * nano, [0, 0, 0], [0, 0, 0.001])
+    # mHe  = Molecule("He", 4.002_602 * u      , 123 * pico        , [0, -0.0001, -0.0001], [0, 0.003, 0.003])
+    # mNe  = Molecule("Ne", 20.179_70 * u      , 38  * pico        , [0, 0.0001, 0.0001], [0, -0.002, -0.002])
+    # mN_2 = Molecule("N2", 14.006_70 * 2 * u  , (0.315 / 2) * nano, [0, 0, 0], [0, 0, 0.001])
+    # mO_2 = Molecule("O2", 15.999_40 * 2 * u  , (0.292 / 2) * nano, [0, 0, 0], [0, 0, 0.001])
+
+    mHe  = Molecule("He", 4.002_602 * u, 1, [0, 0, -5], [0, 0, 2])
+    mNe  = Molecule("Ne", 20.179_70 * u, 1, [0, 0, 5], [0, 0, -2])
 
     # Molecules to add to simulation
-    molecules::Array = [mHe, mNe]
+    molecules::Array{Molecule} = [mHe, mNe]
 
     # Time settings
     delta_t::Number = 0.001
     until::Number = 3
     framerate = 30
 
-    total_frames = framerate * until
     timestamps = 0:delta_t:until
+    total_frames = length(timestamps)
 
     # Observable indicating current frame being generated
     # (Used in the record section)
@@ -93,16 +138,21 @@ function main()
     T = Observable(0.0)
 
     # Compute all positions for all molecules
-    pos_hist::Array{Array} = []
-    for m in molecules
-        push!(pos_hist, computePositions(m, delta_t, until))
-    end
+    # pos_hist::Array{Array} = []
+    # for m in molecules
+    #     push!(pos_hist, computePositions(m, delta_t, until))
+    # end
+    """ pos_hist = [
+        mol1: [10.0, 1.4],
+        mol2: [13.2, 4.5]
+    ]"""
+    pos_hist::Vector = computePositions(molecules, delta_t, until)
 
     # Prepare figure
     fig = Figure(size = (800, 600))
-    xlims = (-0.0001, 0.0001)
-    ylims = (-0.0001, 0.0001)
-    zlims = (-0.0001, 0.0001)
+    xlims = (-10, 10)
+    ylims = (-10, 10)
+    zlims = (-10, 10)
 
     ax = Axis3(
         fig[1, 1], 
@@ -114,21 +164,21 @@ function main()
     )
 
     xs, ys, zs = pos_hist[1]
-    scatter_plot = scatter!(ax, xs, ys, zs, color=:blue, markersize=10)
+    scatter_plot = scatter!(ax, xs, ys, zs, color=1:20, markersize=10)
     
-    record(fig, "out/animation.mp4", 1:total_frames; framerate = framerate) do f
+    record(fig, "out/animation.mp4", 1:framerate:total_frames; framerate = framerate) do f
         frame[] = f
         T[] = timestamps[f]
 
         # Update scatter with current positions
         xs = []; ys = []; zs = []
-        for (i, m) in enumerate(molecules)
+        for (i, m) in enumerate(molecules) # foreach molecule
             x, y, z = pos_hist[i][f]
             push!(xs, x)
             push!(ys, y)
             push!(zs, z)
         end
-        scatter_plot[1] = xs  # x
+        scatter_plot[1] = xs  # xs = [mol1_x, mol2_x, mol3_z] at frame f
         scatter_plot[2] = ys  # y
         scatter_plot[3] = zs  # z
     end
